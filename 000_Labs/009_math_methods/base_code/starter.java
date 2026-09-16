f98da220161ead9ae05b8a982d822e50376c62f5
pyopyofr <pyopyofr@gmail.com>
2026-09-16 19:34:06 +0000

    Lab 009

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("Maximum = " + Math.max((13-6*11),(30%7*(-2))));
+		System.out.println("Square root = " + Math.sqrt(3*8+31%7));
+		System.out.println("Power = " + Math.pow((37/3),(35%21)));
+		System.out.println("Max = " + Math.max((Math.pow(2,(14%3))),(Math.sqrt(2*6))));
 	}
 }
