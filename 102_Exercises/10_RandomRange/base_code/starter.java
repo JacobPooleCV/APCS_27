0cc362ba4e811229b73994ef9b5e1cb43c03ea97
pyopyofr <pyopyofr@gmail.com>
2026-09-17 19:16:33 +0000

    exercise 10

--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:
- *  Date:
+ *	Author:Justin Pyo
+ *  Date: 9/ 17/26
  *	Collaborator(s): 
 */
 
@@ -8,8 +8,31 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+	
+	Scanner range = new Scanner(System.in);
+	System.out.println("Enter 2 numbers to create a range for your random number");
+	System.out.print("Please enter an integer: ");
+	int r1 = range.nextInt();
+	System.out.print("Please enter another integer (bigger than the first): ");
+	int r2 = range.nextInt();
+	System.out.println("");
+	System.out.println("Your range is " + r1 + " to " + r2 + ".");
+	System.out.println("Here are 5 numbers generated in that range.");
+	String seperator = ", ";
+	int generator = (int)(Math.random() * (r2 - r1) + (r1));
+	System.out.print(generator + seperator);
+	generator = (int)(Math.random() * (r2 - r1) + (r1));
+	System.out.print(generator + seperator);
+	generator = (int)(Math.random() * (r2 - r1) + (r1));
+	System.out.print(generator + seperator);
+	generator = (int)(Math.random() * (r2 - r1) + (r1));
+	System.out.print(generator + seperator);
+	generator = (int)(Math.random() * (r2 - r1) + (r1));
+	System.out.println(generator);
+	
+	
+
+
+
 	}
 }
