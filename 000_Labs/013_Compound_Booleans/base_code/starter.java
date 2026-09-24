596778689d7a8bba6932b3e250acad7b26e7254c
pyopyofr <pyopyofr@gmail.com>
2026-09-24 19:44:11 +0000

    Lab 13

--- 000_Labs/013_Compound_Booleans/base_code/starter.java
+++ 000_Labs/013_Compound_Booleans/base_code/starter.java
@@ -1,14 +1,44 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Justin Pyo
+ *  Date: 9/24/26
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+
+		Scanner numbers = new Scanner(System.in);
+		System.out.print("Please enter your first number: ");
+		int int1 = numbers.nextInt();
+		System.out.print("Please enter your second number: ");
+		int int2 = numbers.nextInt();
+		System.out.print("Please enter your third number: ");
+		int int3 = numbers.nextInt();
+		if((int1 > int2) && (int1 > int3)) {
+			System.out.println("Your first number is the largest of the three!");
+			System.out.println("The number was " + int1 + ".");
+		}
+		if((int2 > int1) && (int2 > int3)) {
+			System.out.println("Your second nmber is the largest of the three!");
+			System.out.println("The number was " + int2 + ".");
+
+		}
+		if((int3 > int1) && (int3 > int2)) {
+			System.out.println("Your third number is the largest of the three!");
+			System.out.println("The number was " + int3 + ".");
+		}
+			if((int1 < int2) && (int1 < int3)) {
+				System.out.println("Your first number is the smallest of the three!");
+				System.out.println("The number was " + int1 + ".");
+			}
+			if((int2 < int1) && (int2 < int3)) {
+				System.out.println("Your second nmber is the smallest of the three!");
+				System.out.println("The number was " + int2 + ".");
+			}
+			if((int3 < int1) && (int3 < int2)) {
+				System.out.println("Your third number is the smallest of the three!");
+				System.out.println("The number was " + int3 + ".");
+			}
 	}
 }
